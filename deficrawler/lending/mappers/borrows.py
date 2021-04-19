678260901b2b67a9375f9e9ce5afa9a013dbb828
Ialberquilla <ivan.alberquilla@gmail.com>
2021-04-19 14:38:17 +0200

    dinamic mapping

--- deficrawler/lending/mappers/borrows.py
+++ deficrawler/lending/mappers/borrows.py
@@ -1,72 +1,34 @@
 from deficrawler.lending.borrow import Borrow
+import pkgutil
 
 import json
+import dict_digger
 
 
 class Mappers:
 
     @staticmethod
     def map_borrow(json_data, protocol):
-        if(protocol == 'AAVE'):
-            return Mappers.map_borrow_aave(json_data, protocol)
-        if(protocol == 'COMPOUND'):
-            return Mappers.map_borrow_compound(json_data, protocol)
-        if(protocol == 'MAKER'):
-            return Mappers.map_borrow_maker(json_data, protocol)
-        if(protocol == 'CREAM'):
-            return Mappers.map_borrow_cream(json_data, protocol)
-
-    @staticmethod
-    def map_borrow_aave(json_data, protocol):
         list_borrows = []
+
+        data = pkgutil.get_data(__name__, protocol.lower() + ".json")
+        map_file = json.loads(data.decode())
+
         for ele in json_data:
-            borrow = Borrow(ele['user']['id'],
-                            ele['reserve']['symbol'],
-                            ele['amount'],
-                            ele['timestamp'],
-                            protocol)
-
-            list_borrows.append(borrow.to_dict())
-
-        return list_borrows
-
-    @staticmethod
-    def map_borrow_compound(json_data, protocol):
-        list_borrows = []
-        for ele in json_data:
-            borrow = Borrow(ele['borrower'],
-                            ele['underlyingSymbol'],
-                            ele['amount'],
-                            ele['blockTime'],
-                            protocol)
-
-            list_borrows.append(borrow.to_dict())
-
-        return list_borrows
-
-    @staticmethod
-    def map_borrow_maker(json_data, protocol):
-        list_borrows = []
-        for ele in json_data:
-            borrow = Borrow(ele['owner']['address'],
-                            'DAI',
-                            ele['debt'],
-                            ele['openedAt'],
-                            protocol)
-
-            list_borrows.append(borrow.to_dict())
-
-        return list_borrows
-
-    @staticmethod
-    def map_borrow_cream(json_data, protocol):
-        list_borrows = []
-        for ele in json_data:
-            borrow = Borrow(ele['borrower'],
-                            ele['underlyingSymbol'],
-                            ele['amount'],
-                            ele['blockTime'],
-                            protocol)
+            borrow = Borrow(
+                dict_digger.dig(
+                    ele,
+                    *map_file['borrow']['attributes']['user']),
+                dict_digger.dig(
+                    ele,
+                    *map_file['borrow']['attributes']['token']),
+                dict_digger.dig(
+                    ele,
+                    *map_file['borrow']['attributes']['amount']),
+                dict_digger.dig(
+                    ele,
+                    *map_file['borrow']['attributes']['timestamp']),
+                protocol)
 
             list_borrows.append(borrow.to_dict())
 
