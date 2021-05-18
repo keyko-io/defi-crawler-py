4443c520ccd5d67c1e8823921e8963a99faa6166
Ialberquilla <ivan.alberquilla@gmail.com>
2021-05-18 11:21:58 +0200

    version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     ],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.0.1',
+    version='0.1.0',
     author='keyko.io',
     author_email='root@keyko.io',
     url='https://github.com/keyko-io/defi-crawler-py',
